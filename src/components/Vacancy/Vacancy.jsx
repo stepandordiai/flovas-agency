bb376af71b021ee869416d72fef5aa9fc9cf38e0
Stepan Dordiai <stepandordiai@gmail.com>
2025-05-12 09:42:55 +0200

    v6.0.10 Update Vacancy and data

--- src/components/Vacancy/Vacancy.jsx
+++ src/components/Vacancy/Vacancy.jsx
@@ -7,41 +7,29 @@ const Vacancy = ({ vacancy }) => {
 
 	const { img, place, title } = vacancy;
 
-	// useEffect(() => {
-	// 	document.querySelectorAll(".vacancy__contact-us-link").forEach((btn) => {
-	// 		btn.addEventListener("click", () => {
-	// 			document
-	// 				.querySelector(".contact-us")
-	// 				.classList.add("contact-us--active");
-	// 			document
-	// 				.querySelector(".bg-curtain")
-	// 				.classList.add("bg-curtain--active");
-	// 		});
-	// 	});
-	// }, []);
+	const date = new Date();
+	const dateNow = date.getDate();
+	const monthNow = date.getMonth();
+	const yearNow = date.getFullYear();
+
 	return (
-		<>
-			<div className={"vacancy"}>
-				{/* All images are compressed */}
-				<img className={"vacancy__img"} src={img} alt="" loading="lazy" />
-				<div className={"vacancy__header"}>
-					<p>Місто: {place}</p>
-					<p className={"vacancy__date"}>
-						Опубліковано: 10/05/2025
-						{/* {job.createdAt.slice(0, 10)} */}
-					</p>
-				</div>
-				<p className={"vacancy__title"}>{title}</p>
-				<div className="vacancy__link-container">
-					<NavLink className={"vacancy__link"} to={"job-page"}>
-						{t("more_info_btn")}
-					</NavLink>
-					<a className={"vacancy__tel-link"} href="tel:+420777957290">
-						{t("contact_us_title")}
-					</a>
-				</div>
+		<div className={"vacancy"}>
+			{/* All images are compressed */}
+			<img className={"vacancy__img"} src={img} alt="" loading="lazy" />
+			<p className={"vacancy__date"}>
+				Опубліковано: {dateNow + "/" + (monthNow + 1) + "/" + yearNow}
+			</p>
+			<p>Місто: {place}</p>
+			<p className={"vacancy__title"}>{title}</p>
+			<div className="vacancy__link-container">
+				<NavLink className={"vacancy__link"} to={"job-page"}>
+					{t("more_info_btn")}
+				</NavLink>
+				<a className={"vacancy__link"} href="tel:+420777957290">
+					{t("contact_us_title")}
+				</a>
 			</div>
-		</>
+		</div>
 	);
 };
 
