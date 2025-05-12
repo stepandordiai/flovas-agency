1401b39c226625266127f62f026f6bf4010cbc68
Stepan Dordiai <stepandordiai@gmail.com>
2025-05-12 14:01:52 +0200

    v6.1.1 Update About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,6 +7,13 @@ const About = () => {
 	return (
 		<div className="about" id="about-us">
 			<p className="about__title">{t("about_title")} Flovas</p>
+			<ul className="about__list">
+				<li>Працевлаштування по всій Чехії</li>
+				<li>Для чоловіків жінок та сімейних пар</li>
+				<li>Аванси кожного тижня</li>
+				<li>Надаємо житло</li>
+				<li>Офіційне працевлаштування</li>
+			</ul>
 		</div>
 	);
 };
