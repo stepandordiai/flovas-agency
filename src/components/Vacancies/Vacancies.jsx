3ed06f3889fa2a1b004150c2dc24bd700057843a
Stepan Dordiai <stepandordiai@gmail.com>
2025-05-16 10:49:54 +0200

    v7.1.4 Fix bugs

--- src/components/Vacancies/Vacancies.jsx
+++ src/components/Vacancies/Vacancies.jsx
@@ -5,13 +5,11 @@ import "./Vacancies.scss";
 const Vacancies = ({ vacanciesData }) => {
 	const { t } = useTranslation();
 
-	console.log(vacanciesData);
-
 	return (
 		<div className="vacancies" id="vacancies">
 			<h2 className="vacancies__title">{t("vacancies_title")}</h2>
 			<div className={"vacancies-container"}>
-				{vacanciesData.map((vacancy, index) => {
+				{vacanciesData.reverse().map((vacancy, index) => {
 					return <Vacancy key={index} vacancy={vacancy} />;
 				})}
 			</div>
